71b1e57ee3557c3fd9d9d80b54d24acb42309060
Michael Aichmueller <m.aichmueller@gmail.com>
2024-10-29 14:20:10 +0100

    lose ldd warnings on macos

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,12 @@
 # set(CMAKE_FIND_DEBUG_MODE TRUE)
 
+# macos setting for losing warnings of the kind:
+# ld: warning: search path '/Users/runner/.conan2/p/range0301bf3d76d5d/p/lib' not found
+if(POLICY CMP0142)
+    cmake_policy(SET CMP0142 NEW)
+endif()
+
+
 # Find Boost headers only according to https://cmake.org/cmake/help/latest/module/FindBoost.html
 set(Boost_NO_SYSTEM_PATHS ON)
 find_package(Boost 1.74.0 REQUIRED COMPONENTS iostreams)
@@ -7,9 +14,6 @@ message(STATUS "Found Boost: ${Boost_DIR} (found version ${Boost_VERSION})")
 
 find_package(nauty REQUIRED)
 message(STATUS "Found Nauty: ${NAUTY_LIBRARY} ${NAUTY_INCLUDE_DIR}")
-
-find_package(ZLIB REQUIRED)
-message(STATUS "Found ZLIB: ${ZLIB_INCLUDE_DIR} (found version ${ZLIB_VERSION})")
 
 find_package(loki ${LOKI_MIN_VERSION} REQUIRED COMPONENTS parsers)
 message(STATUS "Found Loki: ${loki_DIR} (found version ${loki_VERSION})")
