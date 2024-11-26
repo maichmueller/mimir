e92961bbeffb3e0831fca6beef39af3dd5244c59
Michael Aichmueller <m.aichmueller@gmail.com>
2024-11-26 11:55:21 +0100

    fix setting up benchmark exe

---
+++ benchmark/profiling.cpp
@@ -0,0 +1,23 @@
+#include "mimir/mimir.hpp"
+
+#include <benchmark/benchmark.h>
+
+using namespace mimir;
+
+static void BM_iw(benchmark::State& state)
+{
+    const auto domain_folder = fs::path(DATA_DIR) / "pushworld";
+    const auto domain_file = domain_folder / "domain.pddl";
+    const auto problem_file = domain_folder / "problem.pddl";
+    const auto state_space = StateSpace::create(domain_file, problem_file).value();
+
+    for (auto _ : state)
+    {
+        IWAlgorithm iw { state_space, 4 };
+        GroundActionList out_plan {};
+        iw.find_solution(out_plan);
+    }
+}
+
+// Register the iw benchmark
+BENCHMARK(BM_iw)->Unit(benchmark::kMillisecond)->Complexity(benchmark::oAuto);
