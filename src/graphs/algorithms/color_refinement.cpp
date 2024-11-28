0575d5185cdd881426a25f139771836d48ce8b40
Michael Aichmueller <m.aichmueller@gmail.com>
2024-11-28 14:30:25 +0100

    use format to print certificate

--- src/graphs/algorithms/color_refinement.cpp
+++ src/graphs/algorithms/color_refinement.cpp
@@ -49,9 +49,9 @@ bool operator==(const Certificate& lhs, const Certificate& rhs)
 
 std::ostream& operator<<(std::ostream& out, const Certificate& element)
 {
-    out << "CertificateColorRefinement(" << "canonical_coloring=" << element.get_canonical_coloring() << ", "
-        << "canonical_compression_function=" << element.get_canonical_compression_function() << ")";
-    return out;
+    return out << fmt::format("CertificateColorRefinement(canonical_coloring=[{}], canonical_compression_function={})",
+                              fmt::join(element.get_canonical_coloring(), ", "),
+                              element.get_canonical_compression_function());
 }
 
 }
