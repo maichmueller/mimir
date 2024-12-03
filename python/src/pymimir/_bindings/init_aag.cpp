833615ded521edf8824084a1462e86addce7c6c6
Michael Aichmueller <m.aichmueller@gmail.com>
2024-12-03 17:20:48 +0100

    remove keep alive

--- python/src/pymimir/_bindings/init_aag.cpp
+++ python/src/pymimir/_bindings/init_aag.cpp
@@ -21,7 +21,7 @@ void init_applicable_action_generator(py::module_& m)
              py::arg("fluent_literals"),
              py::arg("derived_literals"),
              py::arg("pddl_repositories"))
-        .def("ground", &LiftedConjunctionGrounder::ground, py::keep_alive<0, 1>(), py::arg("state"));
+        .def("ground", &LiftedConjunctionGrounder::ground, py::arg("state"));
 
     /* ApplicableActionGenerators */
 
